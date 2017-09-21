0cc2b50aaa1535d5e941cace1fa6d3cb57278b45
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-09-21 12:15:54 -0400

    took away instance vairables, moved all variables to compare_guess method

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,42 +1,42 @@
 def greet
   puts "Hello, guess a number between 1 and 100..."
   print "number choosen: "
-  @rand_num = rand(1..100)
-  @guess = []
 end
 
 def compare_guess
+  rand_num = rand(1..100)
+  guess = []
   for i in 0..4
     number = gets.to_i
     chances = 4 - i
     print "\n"
 
-    if @guess.include?(number)
+    if guess.include?(number)
       puts "You just guessed that SMARTY"
     end
 
-    if number === @rand_num
+    if number === rand_num
       puts "You got it right! Play again later please!"
       break
 
     elsif number === 0
       puts "Invalid input, try again"
 
-    elsif number > @rand_num
+    elsif number > rand_num
       puts "Too high..."
 
-    elsif 0 < number and number < @rand_num
+    elsif 0 < number and number < rand_num
       puts "Too low..."
 
     end
 
     puts "You have #{chances} tries left"
-    @guess.push(number)
+    guess.push(number)
     if chances > 0
       print "Choose new number: "
     elsif chances === 0
       puts "You failed!"
-      puts "The number was #{@rand_num}"
+      puts "The number was #{rand_num}"
     end
   end
 end
