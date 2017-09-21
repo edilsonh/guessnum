ad24061bd2b5b81733603b2dd7314dc8c5399656
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-09-20 21:46:48 -0400

    refactored code to be two methods

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,38 +1,45 @@
-puts "Hello, guess a number between 1 and 100..."
-print "number choosen: "
-rand_num = rand(1..100)
-guess = []
+def greet
+  puts "Hello, guess a number between 1 and 100..."
+  print "number choosen: "
+  @rand_num = rand(1..100)
+  @guess = []
+end
 
-for i in 0..4
-  number = gets.to_i
-  chances = 4 - i
-  print "\n"
+def compare_guess
+  for i in 0..4
+    number = gets.to_i
+    chances = 4 - i
+    print "\n"
 
-  if guess.include?(number)
-    puts "You just guessed that SMARTY"
-  end
+    if @guess.include?(number)
+      puts "You just guessed that SMARTY"
+    end
 
-  if number === rand_num
-    puts "You got it right! Play again later please!"
-    break
+    if number === @rand_num
+      puts "You got it right! Play again later please!"
+      break
 
-  elsif number === 0
-    puts "Invalid input, try again"
+    elsif number === 0
+      puts "Invalid input, try again"
 
-  elsif number > rand_num
-    puts "Too high..."
+    elsif number > @rand_num
+      puts "Too high..."
 
-  elsif 0 < number and number < rand_num
-    puts "Too low..."
+    elsif 0 < number and number < @rand_num
+      puts "Too low..."
 
-  end
+    end
 
-  puts "You have #{chances} tries left"
-  guess.push(number)
-  if chances > 0
-    print "Choose new number: "
-  elsif chances === 0
-    puts "You failed!"
-    puts "The number was #{rand_num}"
+    puts "You have #{chances} tries left"
+    @guess.push(number)
+    if chances > 0
+      print "Choose new number: "
+    elsif chances === 0
+      puts "You failed!"
+      puts "The number was #{@rand_num}"
+    end
   end
 end
+
+greet
+compare_guess
